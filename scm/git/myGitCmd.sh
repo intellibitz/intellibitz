7b9e25b551b1b60ddaa4c1d8c0b725af22d3092c
Muthu Ramadoss <muthu.ramadoss@gmail.com>
2024-10-03 18:23:04 +0530

    merged os/bin/git with scm/git

--- scm/git/myGitCmd.sh
+++ scm/git/myGitCmd.sh
@@ -72,6 +72,17 @@ git pull #if an upstream branch is defined for the current branch, fetch and int
 git pull <shortname> <branch_name> #fetches and integrates changes from the <shortname> remote repository for the specified <branch_name>
 git pull -p #removes remote-tracking branches that correspond to deleted remote branches and fetches and integrates changes from the <shortname> remote repository for the specified <branch_name>
 
+## https://git-scm.com
+## https://git-scm.com/book/en/v2/Git-Basics-Undoing-Things
+## Git commands
+git init
+git clone
+git add
+git commit -m
+git status
+git pull
+git push
+git branch -a
 # https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
 # https://git-scm.com/book/en/v2/Git-Basics-Recording-Changes-to-the-Repository
 cat .gitignore
